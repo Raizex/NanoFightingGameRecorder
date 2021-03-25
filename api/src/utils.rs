84dad0b857c4416d561226cff423507d0716bef6
Tanner <tannerrosner@gmail.com>
2021-03-25 10:22:47 -0600

    Utils rs file

---
+++ api/src/utils.rs
@@ -0,0 +1,20 @@
+use rand::Rng;
+
+//Function generates a unique key
+pub fn generate() -> String{
+
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                            abcdefghijklmnopqrstuvwxyz\
+                            0123456789";
+    const PASSWORD_LEN: usize = 30;
+    let mut rng = rand::thread_rng();
+
+    let password: String = (0..PASSWORD_LEN)
+        .map(|_| {
+            let idx = rng.gen_range(0..CHARSET.len());
+            CHARSET[idx] as char
+        })
+        .collect();
+
+    return password;
+}
